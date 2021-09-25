11e48d23afe2c227fdb118bc100c50e792ca8db3
Andile Jaden Mbele <andilembele020@gmail.com>
2021-09-25 23:41:56 +0200

    JsonBin Configured

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-        target: "http://localhost:5000",
+        target: "https://api.jsonbin.io/b/614f97369548541c29b86451",
         ws: true,
         logLevel: "debug",
         changeOrigin: true,
