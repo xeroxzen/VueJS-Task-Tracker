7d1c85abf2ed0455ad853ee03020733b4fb6048e
Andile Jaden Mbele <andilembele020@gmail.com>
2021-09-25 10:21:34 +0200

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+  <!-- v-on:click=onClick() alternatives -->
+  <!-- @click=onClick() -->
+  <!-- :click=onClick()  -->
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Some styling here */
+</style>
